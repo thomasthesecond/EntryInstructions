ba1275269f5bf39d4fab76e90ab17116596f049f
Tom Cunningham <tcunningham@paramoredigital.com>
2014-10-24 20:59:07 -0500

    Clean up; added editorconfig

--- entryinstructions/fieldtypes/EntryInstructions_InstructionsFieldType.php
+++ entryinstructions/fieldtypes/EntryInstructions_InstructionsFieldType.php
@@ -11,9 +11,5 @@ class EntryInstructions_InstructionsFieldType extends BaseFieldType
     public function getInputHtml($name, $value)
     {
         return craft()->templates->render('entryinstructions/_instructions/layout');
-        // return craft()->templates->render('entryinstructions/_instructions/layout', array(
-        //     'name'  => $name,
-        //     'value' => $value
-        // ));
     }
 }
